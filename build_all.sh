6173320e966c87d1f7e91137cd117b18d5b1a4c8
srinivasaprasad-a <srinivasaprasad.a@gmail.com>
2018-03-20 20:02:00 +0530

    Update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,5 @@
 sudo docker build -t hadoop_ubuntu hadoop_ubuntu
-sudo docker tag hadoop_ubuntu:latest hadoop_ubuntu:2.9
 
 sudo docker build -t hadoop_master hadoop_master
-sudo docker tag hadoop_master:latest hadoop_master:2.9
 
 sudo docker build -t hadoop_slave hadoop_slave
-sudo docker tag hadoop_slave:latest hadoop_slave:2.9
-
